6ce35e965b87eb72f4059546b3c51fb657ec6afb
Anna Caraman <ac@inlead.dk>
2012-12-19 15:20:47 +0000

    case 1901 - Basic theme settings and general styles.

---
+++ js/bmc.js
@@ -0,0 +1,6 @@
+(function($) {
+  $(document).ready(function() {
+    $('.node-type-ding-news .node-details .date-value').after($('.node-type-ding-news .pane-node-content .pane-title'));
+	console.log($('.node-type-ding-news .pane-node-content .pane-title').html());
+  });
+})(jQuery);
