72ccc368a7b43c2a21413f528f63cdc13d6415b8
George Mihailov <gm@inlead.dk>
2013-01-14 15:34:33 +0200

    case-2023: Move the source-spinner

--- mkdru/mkdru.theme.js
+++ mkdru/mkdru.theme.js
@@ -1,8 +1,11 @@
-// Modify the structure of results
+// Modify the structure of results.
 var results = jQuery('<table><thead><tr><th>'+Drupal.t('Title')+'</th><th>'+Drupal.t('Author')+'</th><th>'+Drupal.t('Year')+'</th><th>'+Drupal.t('Listen')+'</th></tr></thead><tbody class="mkdru-result-list"></tbody></table>')
 jQuery('.mkdru-result-list').replaceWith(results)
 
-// Search result item
+// Move status to bottom of search results.
+jQuery('.mkdru-status').css({display:'block'}).appendTo(".mkdru-below");
+
+// Search result item.
 Drupal.theme.prototype.mkdruResult = function(hit, num, detailLink) {
   // Escape if there is no title to avoid showig empty blocks.
   if (hit["md-title"] == undefined) {
@@ -54,7 +57,7 @@ mkdruResourceTitle2ClassName = function(res) {
   return res.match(/(\w+)\s/)[0].toLowerCase();
 };
 
-// Details of found item
+// Details of found item.
 Drupal.theme.prototype.mkdruDetail = function(data, linkBack) {
     return ' ';
 };
@@ -103,12 +106,12 @@ Drupal.theme.prototype.mkdruPager = function (pages, start, current, total, prev
   return Mustache.render(tpl, view);
 };
 
-// Counts
+// Counts.
 Drupal.theme.prototype.mkdruCounts = function(first, last, available, total) {
   return ' '
 };
 
-// Search status
+// Search status.
 Drupal.theme.prototype.mkdruStatus = function(activeClients, clients) {
 
   if (activeClients == 0)
@@ -125,7 +128,7 @@ jQuery('.mkdru-facet-title').css({cursor:'pointer'}).click(function(){
     jQuery(this).siblings('.mkdru-facet').toggle()
 })
 
-// Facet item
+// Facet item.
 Drupal.theme.prototype.mkdruFacet = function (terms, facet, max, selections) {
 
   var view = {
