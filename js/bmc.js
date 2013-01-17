7dd0a41d9077dfd78eb9e1ff145b8d2b6e2fd7e3
George Mihailov <gm@inlead.dk>
2013-01-17 15:09:58 +0200

    case 2022 - code style fixes

--- js/bmc.js
+++ js/bmc.js
@@ -43,7 +43,8 @@
         jQuery('.mkdru-facet-section:has(.mkdru-facet-Date)').insertBefore(jQuery('.mkdru-facet-section:first', container));
         jQuery('.mkdru-facet-section:has(.mkdru-facet-author)').insertBefore(jQuery('.mkdru-facet-section:first', container));
         jQuery('.mkdru-facet-section:has(.mkdru-facet-Type)').insertBefore(jQuery('.mkdru-facet-section:first', container));
-      } else if (container[0] != window.facets_backup[0]) {
+      }
+      else if (container[0] != window.facets_backup[0]) {
         // Restore original order of facets.
         container.replaceWith(window.facets_backup.clone());
       }
@@ -53,54 +54,54 @@
 
   // Initialize custom facet groups.
   function init_facet_groups() {
-      var custom_facets = [{
-        key: 'streaming',
-        name: Drupal.t('Streaming Services'),
-        fragment: {
-          limit_Type: 'album|track|artist'
-        }
-      }, {
-        key: 'videos',
-        name: Drupal.t('Videos'),
-        fragment: {
-          limit_Type: 'video'
-        }
-      }, {
-        key: 'books',
-        name: Drupal.t('Books and sheet music'),
-        fragment: {
-          limit_Type: 'text'
-        }
-      }];
+    var custom_facets = [{
+      key: 'streaming',
+      name: Drupal.t('Streaming Services'),
+      fragment: {
+        limit_Type: 'album|track|artist'
+      }
+    }, {
+      key: 'videos',
+      name: Drupal.t('Videos'),
+      fragment: {
+        limit_Type: 'video'
+      }
+    }, {
+      key: 'books',
+      name: Drupal.t('Books and sheet music'),
+      fragment: {
+        limit_Type: 'text'
+      }
+    }];
 
-      // Create custom facet group on top of search.
-      var uri_fragment = $.deparam.fragment();
-      var facets_ontop = $('<div class="mkdru-facet-section"><h3 class="mkdru-facet-title"></h3><div class="mkdru-facet mkdru-facet-groups"></div></div>');
-      $.each(custom_facets, function(i, e) {
-        $('<a href="">' + e.name + ' (<span class="mkdru-facet-group-amount '+e.key+'">0</span>)</a> ')
-          .fragment($.extend(uri_fragment, e.fragment, {'facet_group': e.key}))
-          .appendTo($('.mkdru-facet-groups', facets_ontop));
-      });
-      $('.mkdru-facet-groups', facets_ontop).append('<a href="' + '/search/node/' + Drupal.settings.mkdru.state.query+'">' + Drupal.t('Editorial') + '</a>');
+    // Create custom facet group on top of search.
+    var uri_fragment = $.deparam.fragment();
+    var facets_ontop = $('<div class="mkdru-facet-section"><h3 class="mkdru-facet-title"></h3><div class="mkdru-facet mkdru-facet-groups"></div></div>');
+    $.each(custom_facets, function(i, e) {
+      $('<a href="">' + e.name + ' (<span class="mkdru-facet-group-amount '+e.key+'">0</span>)</a> ')
+        .fragment($.extend(uri_fragment, e.fragment, {'facet_group': e.key}))
+        .appendTo($('.mkdru-facet-groups', facets_ontop));
+    });
+    $('.mkdru-facet-groups', facets_ontop).append('<a href="' + '/search/node/' + Drupal.settings.mkdru.state.query+'">' + Drupal.t('Editorial') + '</a>');
 
-      // Populate facet amounts.
-      $(document).bind('mkdru.onterm', function(event, data) {
-        $('.mkdru-facet-group-amount').text(0);
-        $.each(data, function(i, e) {
-          $.each(e, function(i, term) {
-            $.each(custom_facets, function(i, e) {
-              var types = e.fragment.limit_Type.split('|');
-              if (jQuery.inArray(term.name, types)) {
-                amount = parseInt($('.mkdru-facet-group-amount.'+e.key).text());
-                amount += parseInt(term.freq);
-                $('.mkdru-facet-group-amount.'+e.key).text(amount);
-              }
-            });
+    // Populate facet amounts.
+    $(document).bind('mkdru.onterm', function(event, data) {
+      $('.mkdru-facet-group-amount').text(0);
+      $.each(data, function(i, e) {
+        $.each(e, function(i, term) {
+          $.each(custom_facets, function(i, e) {
+            var types = e.fragment.limit_Type.split('|');
+            if (jQuery.inArray(term.name, types)) {
+              var amount = parseInt($('.mkdru-facet-group-amount.' + e.key).text());
+              amount += parseInt(term.freq);
+              $('.mkdru-facet-group-amount.' + e.key).text(amount);
+            }
           });
         });
       });
+    });
 
-      return facets_ontop;
-    }
+    return facets_ontop;
+  }
 
 })(jQuery);
