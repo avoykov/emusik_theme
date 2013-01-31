55a4b8edd437a25a04679c6a6e8856485d12fb72
Dragan <di@inlead.dk>
2013-01-31 14:25:36 +0200

    case 2185 - No functional changes, only code formatting fixes.

--- templates/node--ding_event.tpl.php
+++ templates/node--ding_event.tpl.php
@@ -1,38 +1,38 @@
 <?php if ($view_mode == 'teaser'): ?>
-<article<?php print $attributes; ?>>
-  <div class="node-image">
-    <?php print render($content['field_list_image']);?>
-  </div>
-  <div class="node-content">
-    <?php print $user_picture; ?>
-    <?php print render($title_prefix); ?>
-    <?php print render($title_suffix); ?>
-    <div class="category"><?php print render($content['field_editorial_base_n']);?></div>
-    <div class="date"><?php print $date; ?></div>
-    <?php if (!$page && $title): ?>
-    <header>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    </header>
-    <?php endif; ?>
-    <div<?php print $content_attributes; ?>>
-      <?php
+  <article<?php print $attributes; ?>>
+    <div class="node-image">
+      <?php print render($content['field_list_image']); ?>
+    </div>
+    <div class="node-content">
+      <?php print $user_picture; ?>
+      <?php print render($title_prefix); ?>
+      <?php print render($title_suffix); ?>
+      <div class="category"><?php print render($content['field_editorial_base_n']); ?></div>
+      <div class="date"><?php print $date; ?></div>
+      <?php if (!$page && $title): ?>
+        <header>
+          <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+        </header>
+      <?php endif; ?>
+      <div<?php print $content_attributes; ?>>
+        <?php
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
         hide($content['links']);
         print render($content['field_ding_body']);
-      ?>
+        ?>
+      </div>
     </div>
-  </div>
-</article>
+  </article>
 <?php else: ?>
-<article<?php print $attributes; ?>>
-  <?php print render($content['field_main_image']);?>
-  <div class="node-details">
-	<div class="date-value"><?php print $date; ?></div>
-  <h2 class="pane-title"><?php print htmlspecialchars($title); ?></h2>
-	<div class="summary"></div>
-	<div class="author"><?php print t('by '); ?><?php print $name; ?></div>
-	<div class="body"><?php print render($content['field_ding_body']);?></div>
-  </div>
-</article>
+  <article<?php print $attributes; ?>>
+    <?php print render($content['field_main_image']); ?>
+    <div class="node-details">
+      <div class="date-value"><?php print $date; ?></div>
+      <h2 class="pane-title"><?php print htmlspecialchars($title); ?></h2>
+      <div class="summary"></div>
+      <div class="author"><?php print t('by '); ?><?php print $name; ?></div>
+      <div class="body"><?php print render($content['field_ding_body']); ?></div>
+    </div>
+  </article>
 <?php endif; ?>
