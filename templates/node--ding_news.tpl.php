3b04692d1355f8fbdefe3e5e6ef07797623bfe23
Dragan <di@inlead.dk>
2013-01-31 14:24:38 +0200

    case 2185 - Fixing templates and JS. Removed dirty huck with title migration using JS. Indention fix comes with next commit.

--- templates/node--ding_news.tpl.php
+++ templates/node--ding_news.tpl.php
@@ -32,6 +32,7 @@ $field_ding_teaser = field_get_items('node', $variables['elements']['#node'], 'f
   <?php print render($content['field_main_image']);?>
   <div class="node-details">
     <div class="date-value"><?php print $date; ?></div>
+    <h2 class="pane-title"><?php print htmlspecialchars($title); ?></h2>
     <?php if (isset($field_ding_teaser[0]['safe_value'])): ?>
       <div class="summary"><?php print $field_ding_teaser[0]['safe_value']; ?></div>
     <?php endif; ?>
